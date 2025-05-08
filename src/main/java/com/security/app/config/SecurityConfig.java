6533cef3543a7a6e075b0a324bda4db95377d60a
Duván González <duvangonzalez@MacBook-Pro-de-Duvan.local>
2025-05-08 15:39:34 -0500

    iniciar proyecto 'SpringSecurity'

---
+++ src/main/java/com/security/app/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.security.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        
+    }
+
+}
