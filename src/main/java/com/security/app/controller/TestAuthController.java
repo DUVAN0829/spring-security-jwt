734b1f4bf120724f096eabef400643143fc2e4f6
Duván González <duvangonzalez@MacBook-Pro-de-Duvan.local>
2025-05-08 16:54:06 -0500

    refactoriza 'SecurityConfig' y añade directorio 'entities'

--- src/main/java/com/security/app/controller/TestAuthController.java
+++ src/main/java/com/security/app/controller/TestAuthController.java
@@ -1,4 +1,4 @@
-package com.security.app;
+package com.security.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ public class TestAuthController {
 
     @GetMapping("/hello")
     public String hello() {
-        return     "Hello world from hello";
+        return "Hello world from hello";
     }
 
     @GetMapping("/hello-secured")
---
+++ src/main/java/com/security/app/entities/UserEntity.java
@@ -0,0 +1,5 @@
+package com.security.app.entities;
+
+public class UserEntity {
+    
+}
