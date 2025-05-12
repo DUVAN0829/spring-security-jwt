70a9d0eae00fce74d3cb30dccd829fb128cada63
DUVAN0829 <duvancasgonz29@gmail.com>
2025-05-12 16:53:01 -0500

    Añade directorio 'Filter'

---
+++ src/main/java/com/security/app/filter/JwtTokenValidator.java
@@ -0,0 +1,72 @@
+package com.security.app.filter;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.security.app.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    //vars
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION); //* Get the token through the Header
+
+        if (jwtToken != null) {
+
+            jwtToken = jwtToken.substring(7); //* This removes the word Bearer from the token
+
+            DecodedJWT decodedJWT = jwtUtils.validationToken(jwtToken);
+
+            String username = jwtUtils.extractUsername(decodedJWT);
+            String stringAuthorities = jwtUtils.getSpecificClaim(decodedJWT, "authorities").asString();
+
+            Collection<? extends GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(stringAuthorities);
+
+            //Todo: Token values are inserted or saved into the contextHolder
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+
+        }
+
+        filterChain.doFilter(request, response); //* if the token fails, then continue with the next filter and everything fails
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
